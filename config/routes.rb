c95521a21b880fedb56751d952a2ec96a1218832
Harsha Murthy <harshamurthy2@gmail.com>
2017-06-24 16:09:58 +0000

    Add Root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'todo/index', to: 'todo#index'
+  root to: 'todo#index'
+  # get 'todo/index', to: 'todo#index'
   get 'todo/new', to: 'todo#new'
   get 'todo/create', to: 'todo#create'
   get 'todo/edit/:id', to: 'todo#edit'
